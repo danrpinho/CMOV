893bf7e8bfab0868f281147468f5494792826757
danrpinho <up201505302@fe.up.pt>
2019-11-10 23:47:45 +0000

    App now boots and shows register screen

--- client/app/src/main/java/com/example/acmemarket_client/mainmenu/MainMenuActivity.java
+++ client/app/src/main/java/com/example/acmemarket_client/mainmenu/MainMenuActivity.java
@@ -7,9 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.acmemarket_client.R;
+import com.example.acmemarket_client.cart.CartActivity;
+import com.example.acmemarket_client.checkout.CheckoutActivity;
+import com.example.acmemarket_client.history.HistoryActivity;
 import com.example.acmemarket_client.register.RegisterActivity;
 import com.example.acmemarket_client.utils.Constants;
 
@@ -18,14 +22,11 @@ import java.util.Set;
 
 public class MainMenuActivity extends AppCompatActivity {
 
-    Button scanButton, historyButton, cartButton, checkoutButton;
     SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setupButtons();
 
         setContentView(R.layout.activity_main);
         ActionBar bar = getSupportActionBar();
@@ -34,10 +35,25 @@ public class MainMenuActivity extends AppCompatActivity {
             bar.setDisplayShowHomeEnabled(true);
         }
 
-
     }
 
-    private void setupButtons() {
+    public void onScanButtonClick(View view){
+        //TODO Implement QR Code
+    }
+
+    public void onHistoryButtonClick(View view){
+        Intent intent = new Intent(this, HistoryActivity.class);
+        startActivity(intent);
+    }
+
+    public void onCartButtonClick(View view){
+        Intent intent = new Intent(this, CartActivity.class);
+        startActivity(intent);
+    }
+
+    public void onCheckoutButtonClick(View view){
+        Intent intent = new Intent(this, CheckoutActivity.class);
+        startActivity(intent);
     }
 
     @Override
--- client/app/src/main/java/com/example/acmemarket_client/utils/Constants.java
+++ client/app/src/main/java/com/example/acmemarket_client/utils/Constants.java
@@ -3,5 +3,6 @@ package com.example.acmemarket_client.utils;
 public class Constants {
     public class PreferenceKeys{
         public static final String JWT = "jwt";
+        public static final String USER_INFORMATION_PREFERENCES = "userInformation";
     }
 }
