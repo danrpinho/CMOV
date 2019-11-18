2b01a857387429a5f662fb108429194c95dbae93
Rúben <r_torres96@live.com.pt>
2019-11-18 00:59:51 +0000

    Corrected bug on network error messages

---
+++ client/app/src/main/java/com/example/acmemarket_client/model/NetworkLayer/NetworkLayerModels/ErrorBody.java
@@ -0,0 +1,9 @@
+package com.example.acmemarket_client.model.NetworkLayer.NetworkLayerModels;
+
+public class ErrorBody {
+    String message;
+
+    public String getMessage() {
+        return message;
+    }
+}
--- client/app/src/main/java/com/example/acmemarket_client/register/RegisterInteractor.java
+++ client/app/src/main/java/com/example/acmemarket_client/register/RegisterInteractor.java
@@ -5,9 +5,13 @@ import android.os.Handler;
 import androidx.annotation.NonNull;
 
 import com.example.acmemarket_client.model.NetworkLayer.Interactor;
+import com.example.acmemarket_client.model.NetworkLayer.NetworkLayerModels.ErrorBody;
 import com.example.acmemarket_client.model.NetworkLayer.NetworkLayerModels.RegisterRequestBody;
 import com.example.acmemarket_client.model.NetworkLayer.NetworkLayerModels.RegisterResponse;
 import com.example.acmemarket_client.model.User;
+import com.google.gson.Gson;
+
+import java.io.IOException;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -36,6 +40,17 @@ public class RegisterInteractor implements Callback<RegisterResponse> {
 
     @Override
     public void onResponse(Call<RegisterResponse> call, Response<RegisterResponse> response) {
+        if (response.code() != 200) {
+            try {
+                Gson gson = new Gson();
+                ErrorBody error = gson.fromJson(response.errorBody().string(), ErrorBody.class);
+                new Handler().post(() -> errorListener.onError(error.getMessage()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+
         if (!response.body().getMessage().equals("Signup successful")) {
             new Handler().post(() -> errorListener.onError(response.body().getMessage()));
             return;
