44aea82832458a0c4debee9af4e2ba0c35f2f070
danrpinho <up201505302@fe.up.pt>
2019-12-15 19:31:15 +0000

    Created assets folder

---
+++ weather_app/lib/src/ui/theme/theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Themes{
+  //constants for defining the theme.
+  static const int THEME_LIGHT = 0;
+  static const int THEME_DARK  = 1;
+  static const int THEME_GRADIENT = 2;
+
+  static Color getMaxTempColor() => Colors.yellow[900];
+  static Color getMinTempColor() => Colors.lightBlue[400];
+
+  static final _themeLight = ThemeData (
+    backgroundColor: Colors.grey[50],
+    primaryColor: Colors.grey[50],
+  );
+
+  static final _themeDark = ThemeData (
+    primaryColor: Colors.grey[850],
+    backgroundColor: Colors.grey[850],
+  );
+}
--- weather_app/lib/src/ui/widgets/main_info.dart
+++ weather_app/lib/src/ui/widgets/main_info.dart
@@ -1,29 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/src/model/weather.dart';
+import '../theme/theme.dart';
 
-class MainInfo extends StatelessWidget{
+class MainInfo extends StatelessWidget {
   final Weather info;
-  const MainInfo ({Key key, this.info}):super(key:key);
+  const MainInfo({Key key, this.info}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Icon(
-          Icons.cloud,
-          color: Colors.black,
-          size: 45
-        ),
-        Text( // city
-          "PORTO",
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 50,
-            fontWeight: FontWeight.w500,
-          )
-        ),
-        Text( // weather condition
+        Icon(Icons.cloud, color: Colors.black, size: 45),
+        Text(
+            // city
+            "PORTO",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 50,
+              fontWeight: FontWeight.w500,
+            )),
+        Text(
+          // weather condition
           "Cloudy",
           style: TextStyle(
             color: Colors.grey,
@@ -31,32 +29,33 @@ class MainInfo extends StatelessWidget{
           ),
         ),
         createTemperatureWidget(info),
-      
       ],
-
-    
-    
     );
-    
   }
 
   createTemperatureWidget(Weather info) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          "13ºC",
-          style: TextStyle(
-            fontSize: 30, 
-            color: Colors.grey
+    return Container(
+      margin: new EdgeInsets.only(top: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "13ºC",
+            style: TextStyle(fontSize: 30, color: Colors.grey),
           ),
-        ),
-        Column (children: <Widget>[
-          Text("Max: 18ºC"),
-          Text("Min: 9ºC")
-        ],)
-      ],
+          Column(
+            children: <Widget>[
+              Text("Max: 18ºC",
+                style: TextStyle(color: Themes.getMaxTempColor())
+              ),
+              Text(
+                "Min: 9ºC",
+                style: TextStyle(color: Themes.getMinTempColor()),
+              )
+            ],
+          )
+        ],
+      )
     );
   }
-
 }
