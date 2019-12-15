2b23e72e9e399b8134cdf05ac02da5990019aa73
Rúben <r_torres96@live.com.pt>
2019-12-15 17:01:23 +0000

    swiper done

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,55 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:http/http.dart' as http;
 import 'package:weather_app/src/api/open_weather_client.dart';
 import 'package:weather_app/src/ui/widgets/main_info.dart';
-import 'package:http/http.dart' as http;
 
+const double _main_info_margin = 40;
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'WeatherApp',
       theme: ThemeData(
-        // This is the theme of your application.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'ACME Weather  '),
+      home: MyHomePage(
+        title: 'ACME Weather  ',
+        itemCount: 3,
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({Key key, this.title, this.itemCount}) : super(key: key);
 
   final String title;
+  final int itemCount;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
   final clinet = OpenWeatherAPIClient(httpClient: http.Client());
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       clinet.featchWeather("Porto");
       print("hello");
     });
@@ -57,41 +46,24 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            MainInfo(info: null, key: null),
-          ],
-        ),
+      body: new Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            child: new Center(
+                child: Column(
+              children: <Widget>[
+                MainInfo(info: null, key: null),
+              ],
+            )),
+            margin: EdgeInsets.all(_main_info_margin),
+          );
+        },
+        itemCount: widget.itemCount,
+        pagination: new SwiperPagination(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
