57a823c2b82eec39cb93c5fec045674674427f77
João Ferreira <guedes@MBP-de-Joao.lan>
2019-12-15 02:57:44 +0000

    Model of Current Temps, Humidity, Pressure

--- weather_app/lib/src/model/weather.dart
+++ weather_app/lib/src/model/weather.dart
@@ -1,11 +1,82 @@
+//coord
+//coord.lon City geo location, longitude
+//coord.lat City geo location, latitude
+//weather (more info Weather condition codes)
+//weather.id Weather condition id
+//weather.main Group of weather parameters (Rain, Snow, Extreme etc.)
+//weather.description Weather condition within the group
+//weather.icon Weather icon id
+//base Internal parameter
+//main
+//main.temp Temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+//main.pressure Atmospheric pressure (on the sea level, if there is no sea_level or grnd_level data), hPa
+//main.humidity Humidity, %
+//main.temp_min Minimum temperature at the moment. This is deviation from current temp that is possible for large cities and megalopolises geographically expanded (use these parameter optionally). Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+//main.temp_max Maximum temperature at the moment. This is deviation from current temp that is possible for large cities and megalopolises geographically expanded (use these parameter optionally). Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+//main.sea_level Atmospheric pressure on the sea level, hPa
+//main.grnd_level Atmospheric pressure on the ground level, hPa
+//wind
+//wind.speed Wind speed. Unit Default: meter/sec, Metric: meter/sec, Imperial: miles/hour.
+//wind.deg Wind direction, degrees (meteorological)
+//clouds
+//clouds.all Cloudiness, %
+//rain
+//rain.1h Rain volume for the last 1 hour, mm
+//rain.3h Rain volume for the last 3 hours, mm
+//snow
+//snow.1h Snow volume for the last 1 hour, mm
+//snow.3h Snow volume for the last 3 hours, mm
+//dt Time of data calculation, unix, UTC
+//sys
+//sys.type Internal parameter
+//sys.id Internal parameter
+//sys.message Internal parameter
+//sys.country Country code (GB, JP etc.)
+//sys.sunrise Sunrise time, unix, UTC
+//sys.sunset Sunset time, unix, UTC
+//timezone Shift in seconds from UTC
+//id City ID
+//name City name
+//cod Internal parameter
+
+import 'dart:ffi';
+
 class Weather {
   String name;
   int cityId;
   //int weatherId;
+  double temp;
+  double minTemp;
+  double maxTemp;
+  double feelsLike;
 
-  Weather({this.cityId, this.name});
+  int pressure;
+  int humidity;
+
+  Weather(
+      {this.cityId,
+      this.name,
+      this.temp,
+      this.maxTemp,
+      this.minTemp,
+      this.humidity,
+      this.feelsLike,
+      this.pressure});
 
   static Weather mapFromJson(Map<String, dynamic> json) {
-    return Weather(cityId: json['id'], name: json['name']);
+    return Weather(
+        cityId: json['id'],
+        name: json['name'],
+
+        //main.temp Temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+        temp: json['main']['temp'],
+        //main.pressure Atmospheric pressure (on the sea level, if there is no sea_level or grnd_level data), hPa
+        pressure: json['main']['pressure'],
+        //main.humidity Humidity, %
+        humidity: json['main']['humidity'],
+        //main.temp_min Minimum temperature at the moment. This is deviation from current temp that is possible for large cities and megalopolises geographically expanded (use these parameter optionally). Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+        minTemp: json['main']['temp_min'],
+        //main.temp_max Maximum temperature at the moment. This is deviation from current temp that is possible for large cities and megalopolises geographically expanded (use these parameter optionally). Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+        maxTemp: json['main']['temp_max']);
   }
 }
