757bc6159c2c6a55e2daedb67f80d80ebc863f51
danrpinho <up201505302@fe.up.pt>
2019-12-17 19:10:11 +0000

    Working on gradients

--- weather_app/lib/src/ui/theme/theme.dart
+++ weather_app/lib/src/ui/theme/theme.dart
@@ -9,6 +9,12 @@ class Themes {
 
   static get lightTheme => _themeLight;
   static get darkTheme => _themeDark;
+  static get gradientThemeSunny => _gradientSunny;
+  static get gradientThemeRainyStormy => _gradientRainy;
+  static get gradientThemeFoggySnowy => _gradientFoggy;
+  static get gradientThemeUnknown => _gradientUnknown;
+
+  static get gradientBackgroundSunny => _backgroundSunny;
 
   static Color getMaxTempColor() => Colors.yellow[900];
   static Color getMinTempColor() => Colors.lightBlue[400];
@@ -24,7 +30,8 @@ class Themes {
     ),
     iconTheme: IconThemeData(
       color: Colors.grey[700]
-    )
+    ),
+    buttonColor: Colors.grey[850],
   );
 
   static final _themeDark = ThemeData(
@@ -38,6 +45,76 @@ class Themes {
     ),
     iconTheme: IconThemeData(
       color: Colors.grey[100]
+    ),
+    buttonColor: Colors.grey[50],
+  );
+
+  static final _gradientSunny = ThemeData(
+    primaryColor: Colors.lightBlue[400],
+    backgroundColor: Colors.lightBlue[400],
+    primaryTextTheme: TextTheme(
+      title: GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+      body1: GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+      body2: TextStyle(color: Colors.white),
+      subtitle: GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.white)),
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.white
+    ),
+    buttonColor: Colors.white,
+  );
+
+  static final _gradientRainy = ThemeData(
+    primaryColor: Colors.indigo[400],
+    backgroundColor: Colors.indigo[400],
+    primaryTextTheme: TextTheme(
+      title: GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+      body1: GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+      body2: TextStyle(color: Colors.white),
+      subtitle: GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.white)),
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.white
+    ),
+    buttonColor: Colors.white,
+  );
+
+  static final _gradientFoggy = ThemeData(
+    primaryColor: Colors.blueGrey[400],
+    backgroundColor: Colors.blueGrey[400],
+    primaryTextTheme: TextTheme(
+      title: GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+      body1: GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+      body2: TextStyle(color: Colors.white),
+      subtitle: GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.white)),
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.white
+    ),
+    buttonColor: Colors.white,
+  );
+
+  static final _gradientUnknown = ThemeData(
+    primaryColor: Colors.grey,
+    backgroundColor: Colors.grey,
+    primaryTextTheme: TextTheme(
+      title: GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+      body1: GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+      body2: TextStyle(color: Colors.white),
+      subtitle: GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.white)),
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.white
+    ),
+    buttonColor: Colors.white,
+  );
+
+  static final _backgroundSunny = BoxDecoration(
+    gradient: LinearGradient(
+      colors: [Colors.lightBlue[400], Colors.lightBlue[900]],
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
     )
   );
+
 }
