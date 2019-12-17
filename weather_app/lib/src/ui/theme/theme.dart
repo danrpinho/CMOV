1137754133a8f884eb650eaf6de297a17319a421
danrpinho <up201505302@fe.up.pt>
2019-12-17 16:18:29 +0000

    Defined light and dark themes

--- weather_app/lib/src/ui/theme/theme.dart
+++ weather_app/lib/src/ui/theme/theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Themes {
   //constants for defining the theme.
@@ -15,10 +16,28 @@ class Themes {
   static final _themeLight = ThemeData(
     backgroundColor: Colors.grey[50],
     primaryColor: Colors.grey[50],
+    primaryTextTheme: TextTheme(
+      body1: GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.grey[700], fontWeight: FontWeight.bold)),
+      body2: TextStyle(color: Colors.grey),
+      title: GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.grey[850], fontWeight: FontWeight.bold)),
+      subtitle: GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.grey[700])),
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.grey[700]
+    )
   );
 
   static final _themeDark = ThemeData(
     primaryColor: Colors.grey[850],
     backgroundColor: Colors.grey[850],
+    primaryTextTheme: TextTheme(
+      title: GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.grey[50], fontWeight: FontWeight.bold)),
+      body1: GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.grey[100], fontWeight: FontWeight.bold)),
+      body2: TextStyle(color: Colors.grey),
+      subtitle: GoogleFonts.montserrat(textStyle: TextStyle(color: Colors.grey[100])),
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.grey[100]
+    )
   );
 }
